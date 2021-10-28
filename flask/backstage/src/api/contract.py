c74964026120fe0a591acee5a63f4ca642490b07
Brian Zbriger <brian.zbr@gmail.com>
2021-10-27 20:30:23 -0400

    import abort for contract

--- flask/backstage/src/api/contract.py
+++ flask/backstage/src/api/contract.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, abort
 from ..models import Contract, db
 
 bp = Blueprint('contract', __name__, url_prefix='/contract')
